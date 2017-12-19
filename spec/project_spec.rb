e7a3db93c1b02e19b37b0825a27bd4c1672424cc
sinsoku <sinsoku.listy@gmail.com>
2017-12-19 20:17:36 +0900

    :sparkles: Add test case to check the repo name

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -2,8 +2,15 @@
 require 'spec_helper'
 
 describe 'data/github_urls.yml' do
+  let(:yaml) { YAML.load_file(GemsComparator::GemInfo::GITHUB_URLS_PATH) }
+
   it 'should sort by gem name' do
-    yaml = YAML.load_file(GemsComparator::GemInfo::GITHUB_URLS_PATH)
     expect(yaml.keys.join("\n")).to eq yaml.keys.sort.join("\n")
+  end
+
+  it 'should only inclue repo names' do
+    yaml.values.each do |full_name|
+      expect(full_name).to match(/[\w-]+\/[\w-]+/)
+    end
   end
 end
