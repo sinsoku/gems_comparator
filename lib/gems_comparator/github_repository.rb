1bd5c6f370ebceaf548f9edb89803f793c22dff7
sinsoku <sinsoku.listy@gmail.com>
2018-09-24 19:42:49 +0900

    Support the special pattern such as version-x.y.z

--- lib/gems_comparator/github_repository.rb
+++ lib/gems_comparator/github_repository.rb
@@ -18,20 +18,22 @@ module GemsComparator
       head_tag = find_tag(head)
 
       if base_tag && head_tag
-        "#{@repo.url}/compare/#{base_tag.name}...#{head_tag.name}"
+        "#{@repo.url}/compare/#{base_tag}...#{head_tag}"
       elsif base_tag
-        "#{@repo.url}/compare/#{base_tag.name}...master"
+        "#{@repo.url}/compare/#{base_tag}...master"
       end
     end
 
     private
 
-    def tags
-      @tags ||= client.tags(@repo)
+    def tag_names
+      @tag_names ||= client.tags(@repo).map(&:name)
     end
 
     def find_tag(name)
-      tags.find { |tag| ["v#{name}", name].include?(tag.name) }
+      return if name.empty?
+
+      tag_names.find { |tag_name| tag_name.include?(name) }
     end
 
     def client
--- spec/gems_comparator/github_repository_spec.rb
+++ spec/gems_comparator/github_repository_spec.rb
@@ -50,6 +50,17 @@ module GemsComparator
         subject { repo.compare('0.1.0', '0.2.0') }
         it { is_expected.to be_nil }
       end
+
+      context 'when the tag name is a special pattern' do
+        let(:tags) { [{ name: 'version-0.1.0' }, { name: 'version-0.2.0' }] }
+
+        before do
+          stub_octokit(:get, '/repos/sinsoku/gems_comparator/tags')
+            .to_return(body: JSON.dump(tags))
+        end
+        subject { repo.compare('0.1.0', '0.2.0') }
+        it { is_expected.to eq "#{url}/compare/version-0.1.0...version-0.2.0" }
+      end
     end
   end
 end
